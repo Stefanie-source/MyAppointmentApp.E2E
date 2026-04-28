5a98a79df0f380d87ce58a343fa31f6d36888598
Steffie-stack <s.hofmann.1988@web.de>
2026-04-28 12:06:46 +0200

    Restructure project and add tests

---
+++ MyAppointmentApp.E2ETests/SeleniumTestBase.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAppointmentApp.E2ETests
+{
+    public class SeleniumTestBase : IDisposable
+    {
+
+        protected IWebDriver Driver { get;  set; }
+
+        public SeleniumTestBase()
+        {
+            var options = new ChromeOptions ();
+            options.AddArgument("--headless");
+
+            Driver=new ChromeDriver(options);
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+        public void Dispose()
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+    }
+}
